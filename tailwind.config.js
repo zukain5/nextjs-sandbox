7afab5eadaa76e05ad61b9a76435ad8e7c4ec3c7
Kazuki Arai <zukain5@gmail.com>
2023-09-18 22:09:44 +0900

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: 'jit',
+    purge: {
+        content: [
+            './pages/**/*.{js,jsx,ts,tsx}',
+        ],
+        options: {
+            safelist: {
+                standard: [/^bg-/, /^text-/],
+            },
+        },
+    },
+    content: [],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
