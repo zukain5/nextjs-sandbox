049fe193e53eca3d5b2a33ca6531d62ea32514e2
Kazuki Arai <zukain5@gmail.com>
2023-09-20 00:58:49 +0900

    delete extra process

--- pages/api/todos.tsx
+++ pages/api/todos.tsx
@@ -38,21 +38,5 @@ export default function handler(
       console.error('Error reading and parsing JSON file:', error);
       res.status(500).json({ error: 'Failed to read JSON file' });
     }
-  } else if (req.method === 'DELETE') {
-    const { id } = req.query;
-    if (!id) {
-      res.status(400).json({ error: 'Missing id' });
-      return;
-    }
-    try {
-      const fileContents = fs.readFileSync(todosFilePath, 'utf8');
-      const todos = JSON.parse(fileContents) as Todo[];
-      const newTodos = todos.filter((todo) => todo.id !== id);
-      fs.writeFileSync(todosFilePath, JSON.stringify(newTodos));
-      res.status(204).end();
-    } catch (error) {
-      console.error('Error reading and parsing JSON file:', error);
-      res.status(500).json({ error: 'Failed to read JSON file' });
-    }
   }
 }
